9ff6a1a56206068a2ceb2b7735bf9f6ef78db5d0
QaisarGit <qaisar.fareed@gmail.com>
2026-03-14 17:05:31 +0530

    request Specification 001

--- src/test/java/restASSUREDpack/restASSUREDclass.java
+++ src/test/java/restASSUREDpack/restASSUREDclass.java
@@ -1,13 +1,26 @@
 package restASSUREDpack;
 
 import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class restASSUREDclass {
 	@Test
 	public void  restAssured() {
 	
+		RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books"; 
+		// Get the RequestSpecification of the request to be sent to the server. 
+		RequestSpecification httpRequest = RestAssured.given(); 
+		// specify the method type (GET) and the parameters if any. 
+		//In this case the request does not take any parameters 
+		Response response = httpRequest.request(Method.GET, "");
+		// Print the status and message body of the response received from the server 
+		System.out.println("Status received => " + response.getStatusLine()); 
+		System.out.println("Response=>" + response.prettyPrint());
+    }
 	
-	System.out.println(" rest assured get ");
 }
 
-}
+
