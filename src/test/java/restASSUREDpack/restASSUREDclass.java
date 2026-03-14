73cf0bace8a2d6bbade87dc3c5bf4c4ec06ed7b3
QaisarGit <qaisar.fareed@gmail.com>
2026-03-14 17:18:57 +0530

    02

--- src/test/java/restASSUREDpack/restASSUREDclass.java
+++ src/test/java/restASSUREDpack/restASSUREDclass.java
@@ -7,8 +7,8 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class restASSUREDclass {
-	@Test
-	public void  restAssured() {
+	//@Test (description="get method ")
+	public void  restAssured() { 
 	
 		RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books"; 
 		// Get the RequestSpecification of the request to be sent to the server. 
@@ -18,9 +18,28 @@ public class restASSUREDclass {
 		Response response = httpRequest.request(Method.GET, "");
 		// Print the status and message body of the response received from the server 
 		System.out.println("Status received => " + response.getStatusLine()); 
+		System.out.println("Status code  received => " + response.statusCode()); 
+
 		System.out.println("Response=>" + response.prettyPrint());
     }
+	@Test(description="get method 1")
 	
+	public void getMeth() {
+		RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+
+		// Get the RequestSpecification of the request that is to be sent
+		// to the server.
+		RequestSpecification httpRequest = RestAssured.given();
+
+		// Call RequestSpecification.get() method to get the response.
+		// Make sure you specify the resource name.
+		Response response = httpRequest.get("");
+
+		// Response.asString method will directly return the content of the body
+		// as String.
+		System.out.println("Response Body is =>  " + response.asString());
+		System.out.println("status code and \n" +response.statusCode() +"\nstatus codeline \n" +response.statusLine());
+	}
 }
 
 
