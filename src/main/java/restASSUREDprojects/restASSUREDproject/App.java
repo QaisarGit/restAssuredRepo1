3d179107c446603481957340d3b5c22b965c8c81
QaisarGit <qaisar.fareed@gmail.com>
2026-03-14 16:18:32 +0530

    git- rest assured 001

---
+++ src/main/java/restASSUREDprojects/restASSUREDproject/App.java
@@ -0,0 +1,10 @@
+package restASSUREDprojects.restASSUREDproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/restASSUREDpack/restASSUREDclass.java
@@ -0,0 +1,13 @@
+package restASSUREDpack;
+
+import org.testng.annotations.Test;
+
+public class restASSUREDclass {
+	@Test
+	public void  restAssured() {
+	
+	
+	System.out.println(" rest assured get ");
+}
+
+}
