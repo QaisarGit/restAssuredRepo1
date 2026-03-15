9734b7595e58453444f6e1e7f38620a712dcd329
QaisarGit <qaisar.fareed@gmail.com>
2026-03-15 23:54:41 +0530

    post method

--- src/test/java/restASUUREDMethods/postMETHOD.java
+++ src/test/java/restASUUREDMethods/postMETHOD.java
@@ -1,5 +1,12 @@
 package restASUUREDMethods;
 
+import org.testng.annotations.Test;
+
 public class postMETHOD {
 
+	@Test (description="post method")
+	public void postMethod() {
+		
+		System.out.println("post method ============ ");
+	}
 }
